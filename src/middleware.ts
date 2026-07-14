5dad5cf30d1ffc4c2824d60d696b82af7e8f0a4c
headerr0001-glitch <headerr0001@gmail.com>
2026-07-14 09:54:45 +0530

    Removed nodejs runtime

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,6 @@
 // @ts-ignore
 import { clerkMiddleware } from '@clerk/nextjs/server';
 
-// 1. ADD THIS LINE: Force Vercel to bypass the Edge network
-export const runtime = 'nodejs';
-
 export default clerkMiddleware(async (auth: any, req: any) => {
   if (req.nextUrl.pathname.startsWith('/admin')) {
     await auth.protect(); 
